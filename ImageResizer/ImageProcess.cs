55c192a6601afce3ae40d6dfa9ba296eef687c1c
Eric Lin <funnytseric@divc.csie.ndhu.edu.tw>
2019-09-30 15:26:50 +0800

    first version: simply added an async function to the ImageProcess class and use WhenAll to await the result, the improvement rate is around 30%

--- ImageResizer/ImageProcess.cs
+++ ImageResizer/ImageProcess.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ImageResizer
 {
@@ -56,6 +57,42 @@ namespace ImageResizer
                 string destFile = Path.Combine(destPath, imgName + ".jpg");
                 processedImage.Save(destFile, ImageFormat.Jpeg);
             }
+        }
+
+        /// <summary>
+        /// 非同步版圖片的縮放作業
+        /// </summary>
+        /// <param name="sourcePath">圖片來源目錄路徑</param>
+        /// <param name="destPath">產生圖片目的目錄路徑</param>
+        /// <param name="scale">縮放比例</param>
+        public async Task ResizeImageAsync(string sourcePath, string destPath, double scale)
+        {
+            var allFiles = FindImages(sourcePath);
+            var taskList = new List<Task>();
+
+            foreach (var filePath in allFiles)
+            {
+                taskList.Add(Task.Run(()=>
+                {
+                    Image imgPhoto = Image.FromFile(filePath);
+                    string imgName = Path.GetFileNameWithoutExtension(filePath);
+
+                    int sourceWidth = imgPhoto.Width;
+                    int sourceHeight = imgPhoto.Height;
+
+                    int destionatonWidth = (int)(sourceWidth * scale);
+                    int destionatonHeight = (int)(sourceHeight * scale);
+
+                    Bitmap processedImage = processBitmap((Bitmap)imgPhoto,
+                        sourceWidth, sourceHeight,
+                        destionatonWidth, destionatonHeight);
+
+                    string destFile = Path.Combine(destPath, imgName + ".jpg");
+                    processedImage.Save(destFile, ImageFormat.Jpeg);
+                }));
+            }
+
+            await Task.WhenAll(taskList);
         }
 
         /// <summary>
--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -9,21 +10,60 @@ namespace ImageResizer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            int numTotalRound = 5;
+            double avgNormal = 0.0;
+            double avgAsync = 0.0;
             string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
             string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
-
+            List<long> normalProcessTimes = new List<long>();
+            List<long> asyncProcessTimes = new List<long>();
             ImageProcess imageProcess = new ImageProcess();
-
-            imageProcess.Clean(destinationPath);
-
             Stopwatch sw = new Stopwatch();
-            sw.Start();
-            imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
-            sw.Stop();
 
-            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+            //執行 N 次普通轉換，並記下平均
+            Console.WriteLine($"開始執行一般處理，共 {numTotalRound} 次");
+            for (int i=0; i< numTotalRound; i++)
+            {
+                imageProcess.Clean(destinationPath);
+
+                Console.Write($"正在執行第 { i+1 } 次處理...");
+                sw.Restart();
+                imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+                sw.Stop();
+                Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
+
+                normalProcessTimes.Add(sw.ElapsedMilliseconds);
+                System.GC.Collect();
+            }
+
+            avgNormal = normalProcessTimes.Average(x => x);
+            Console.WriteLine($"平均處理時間：{avgNormal} ms");
+
+            //執行 N 次非同步轉換，並記下平均
+            Console.WriteLine($"\n\n開始執行非同步處理，共 {numTotalRound} 次");
+            for (int i = 0; i < numTotalRound; i++)
+            {
+                imageProcess.Clean(destinationPath);
+
+                Console.Write($"正在執行第 { i + 1 } 次處理...");
+                sw.Restart();
+                await imageProcess.ResizeImageAsync(sourcePath, destinationPath, 2.0);
+                sw.Stop();
+                Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
+
+                asyncProcessTimes.Add(sw.ElapsedMilliseconds);
+                System.GC.Collect();
+            }
+
+            avgAsync = asyncProcessTimes.Average(x => x);
+            Console.WriteLine($"平均處理時間：{avgAsync} ms");
+
+            Console.WriteLine($"\n\n比較結果：普通處理平均 {avgNormal} ms; " +
+                $"非同步處理平均 {avgAsync} ms; " +
+                $"改善率 {(avgNormal - avgAsync) / avgNormal * 100, 0:F3} %");
+            Console.ReadKey();
         }
     }
 }
